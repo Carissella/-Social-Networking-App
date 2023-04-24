953f51120707b25a10b9d592682a078ae9695f24
Carisse Barr <carissebarr.swe@gmail.com>
2023-04-24 12:42:18 -0400

    added routes to get,put, and delete thought data

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const Router = require("express").Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+//   } = require("thought controller file path");
+  
+  router.route("/").get(getAllThoughts).post(createThought);
+  
+  router
+    .route("/:thoughtId")
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+  
+  router.route('/:thoughtId/reactions').post(addReaction);
+  
+  router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+  
+  module.exports = router;
